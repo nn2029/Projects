284d8a6753cd50cbc673240225b26a01e49e2241
Nasss <nn2029@hw.ac.uk>
2023-05-15 18:16:41 +0400

    update

--- Web Dev/Portfolio/home/views.py
+++ Web Dev/Portfolio/home/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse("This is my homepage")
+
+
+def about(request):
+    return HttpResponse("This is my about page")
+
+
+def projects(request):
+    return HttpResponse("This is my projects page")
+
+
+def contact(request):
+    return HttpResponse("This is my contact page")
+
+
+
