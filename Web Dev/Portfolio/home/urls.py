42a16b6a5e815242951577e8715963a37d628604
Nasss <nn2029@hw.ac.uk>
2023-05-23 18:17:40 +0400

    file update

--- Web Dev/Portfolio/home/urls.py
+++ Web Dev/Portfolio/home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('projects', views.projects, name='projects'),
-    path('contact', views.contact, name='contact')
+    path('contact', views.contact, name='contact'),
+    path('blog', views.blog, name='blog')
 ]
--- Web Dev/Portfolio/home/views.py
+++ Web Dev/Portfolio/home/views.py
@@ -3,19 +3,21 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is my homepage")
+    context = {"name": "Nasir", "title": "Portfolio"}
+    return render(request, "home.html")
 
 
 def about(request):
-    return HttpResponse("This is my about page")
+    return render(request, "about.html")
 
 
 def projects(request):
-    return HttpResponse("This is my projects page")
+    return render(request, "projects.html")
 
 
 def contact(request):
-    return HttpResponse("This is my contact page")
+    return render(request, "contact.html")
 
 
-
+def blog(request):
+    return render(request, "blog.html")
